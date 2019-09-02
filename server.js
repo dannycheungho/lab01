e8890a06b8c95c07d13d381169c7899c6a124024
Danny <day@example.com>
2019-09-02 12:05:30 +0800

    updated API key

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require('express');
 const fetch = require('node-fetch');
 
 const app = express();
-const APIKEY = "";  //signup at api.openweathermap.org and obtain an API Key
+const APIKEY = "f943e61ea3c3fe0e9eadefed9ae0207d";  //signup at api.openweathermap.org and obtain an API Key
 
 var options = {
     host: 'api.openweathermap.org',
